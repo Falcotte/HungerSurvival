212bea72cd06fac929363f9b30f379d13461a53a
Ali Ünal <0033484@gmail.com>
2017-11-17 10:35:48 +0300

    Movement Implementation Began

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    BoxCollider2D boxCollider;
+    Rigidbody2D rb2D;
+    float inverseMoveTime;
+
+	protected virtual void Start () {
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+		
+	}
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D Hit)
+    {
+        Vector2 Start = transform.position;
+        Vector2 End = Start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        Hit = Physics2D.Linecast(Start, End, blockingLayer);
+        boxCollider.enabled = true;
+
+        if(Hit.transform==null)
+        {
+            StartCoroutine(SmoothMovement(End));
+            return true;
+        }
+
+        return false;
+    }
+    
+    protected IEnumerator SmoothMovement(Vector3 End)
+    {
+        float sqrRemainingDistance = (transform.position - End).sqrMagnitude;
+
+        while(sqrRemainingDistance>float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, End, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - End).sqrMagnitude;
+            yield return null;
+        }
+    }
+    protected virtual void AttemptMove<T>(int xDir, int yDir)
+
+        where T : Component
+    {
+        RaycastHit2D Hit;
+        bool canMove = Move(xDir, yDir, out Hit);
+
+        if (Hit.transform == null)
+            return;
+
+        T hitComponent = Hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+	
+    protected abstract void OnCantMove<T>(T Component)
+    
+        where T : Component;
+    
+}
---
+++ Assets/Scripts/Walls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walls : MonoBehaviour {
+
+    public Sprite dmgSprite;
+    public int Hp = 4;
+
+    SpriteRenderer spriteRenderer;
+
+	void Awake () {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+		
+	}
+
+    public void DamageWall(int Loss)
+    {
+        spriteRenderer.sprite = dmgSprite;
+        Hp -= Loss;
+
+        if (Hp <= 0)
+            gameObject.SetActive(false);
+    }
+
+}
